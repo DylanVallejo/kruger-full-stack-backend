4ea24d7a33d0b16ff38e97c26cbe7d46e05890b8
Edwin Pinchao <miguepinchao1992@gmail.com>
2023-01-22 10:37:22 -0600

    "gateway microservices"

---
+++ gateway-microservice/src/test/java/com/fastfood/gatewaymicroservice/GatewayMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastfood.gatewaymicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
