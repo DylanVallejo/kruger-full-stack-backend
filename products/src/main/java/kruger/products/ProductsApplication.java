956d59864b229edcce16f8776d9d8439dc7c16ae
Edwin Pinchao <miguepinchao1992@gmail.com>
2023-01-26 21:35:31 -0600

    "Cambio de nombre products, dependencias enable"

--- products/src/main/java/kruger/products/ProductsApplication.java
+++ products/src/main/java/kruger/products/ProductsApplication.java
@@ -3,12 +3,14 @@ package kruger.products;
 import kruger.products.Audit.AuditorAwareImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
+@EnableDiscoveryClient
 public class ProductsApplication {
 
 	@Bean
