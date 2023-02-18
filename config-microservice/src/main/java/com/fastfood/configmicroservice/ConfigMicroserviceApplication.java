eb87bec3e86f8b5a622a918596f434fa95f49e34
Edwin Pinchao <miguepinchao1992@gmail.com>
2023-02-17 19:49:21 -0600

    "Comit cofig-server microservicio "

--- config-microservice/src/main/java/com/fastfood/configmicroservice/ConfigMicroserviceApplication.java
+++ config-microservice/src/main/java/com/fastfood/configmicroservice/ConfigMicroserviceApplication.java
@@ -3,8 +3,9 @@ package com.fastfood.configmicroservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-@EnableConfigServer
+
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigMicroserviceApplication {
 
 	public static void main(String[] args) {
